bdf17dad33e1ad98b4ccd5c0190ff840ebbcb0cf
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-09-04 02:41:53 +0200

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,19 +1,33 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react'
 
-export const useFetch = (url) => {
+export const useFetch = (url, method = 'GET') => {
   const [data, setData] = useState(null)
   const [isPending, setIsPending] = useState(false)
   const [error, setError] = useState(null)
+  const [options, setOptions] = useState(null)
+  
+  const postData = (postData) => {
+    setOptions({
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(postData),
+    })
+  }
 
   useEffect(() => {
     const controller = new AbortController()
 
-    const fetchData = async () => {
+    const fetchData = async (fetchOptions) => {
       setIsPending(true)
-      
+
       try {
-        const res = await fetch(url, { signal: controller.signal })
-        if(!res.ok) {
+        const res = await fetch(url, {
+          ...fetchOptions,
+          signal: controller.signal,
+        })
+        if (!res.ok) {
           throw new Error(res.statusText)
         }
         const data = await res.json()
@@ -22,8 +36,8 @@ export const useFetch = (url) => {
         setData(data)
         setError(null)
       } catch (err) {
-        if (err.name === "AbortError") {
-          console.log("the fetch was aborted")
+        if (err.name === 'AbortError') {
+          console.log('the fetch was aborted')
         } else {
           setIsPending(false)
           setError('Could not fetch the data')
@@ -31,13 +45,19 @@ export const useFetch = (url) => {
       }
     }
 
+    if (method === 'GET') {
+      fetchData()
+    }
+    if (method === 'POST' && options) {
+      fetchData(options)
+    }
+
     fetchData()
 
     return () => {
       controller.abort()
     }
+  }, [url, options, method])
 
-  }, [url])
-
-  return { data, isPending, error }
+  return { data, isPending, error, postData }
 }
